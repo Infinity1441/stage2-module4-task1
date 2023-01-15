a685f28c27ab531606199aa23a29646491400f90
Infinity1441 <jumabekova.baxtigul@gmail.com>
2023-01-15 16:35:39 +0500

    DP5

--- task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
+++ task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
@@ -1,11 +1,12 @@
 package com.mjc.stage2;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ThreadSafeSingleton {
     // Write your code here!
-    private static ThreadSafeSingleton instance = null;
-
-    private ThreadSafeSingleton() {
-    }
+    private static ThreadSafeSingleton instance;
 
     public synchronized static ThreadSafeSingleton getInstance() {
         if (instance == null) {
